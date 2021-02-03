2fab7611cd9566d17f729935feec095558ee7c1a
andrus1st <andrus1@gmail.com>
2021-02-03 16:06:09 -0300

    ¨con cambios para Herouku

--- app.js
+++ app.js
@@ -12,4 +12,6 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/register.html"))});
 
-app.listen(3000, ()=> console.log("servidor corriendo"));
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Server running, port 3000');
+});
