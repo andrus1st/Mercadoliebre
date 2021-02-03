1664cda4acf9daa8fb60c42789f09c9278211259
andrus1st <andrus1@gmail.com>
2021-02-03 14:37:46 -0300

    MercadoLiebre

--- app.js
+++ app.js
@@ -6,4 +6,10 @@ app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.get('/', (req, res)=>{res.sendFile(path.resolve(__dirname, "./views/home.html"))});
 
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))});
+
 app.listen(3000, ()=> console.log("servidor corriendo"));
